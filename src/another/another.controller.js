6f9b3e18101903342afaca57a8b668a6f21e6af3
Murilo Vicente da Silva <murilovicen@gmail.com>
2018-07-12 22:43:37 -0300

    some changes

--- src/another/another.controller.js
+++ src/another/another.controller.js
@@ -5,9 +5,20 @@ class AnotherCtrl {
 	 * @param {string} numero - Numero 1
 	 * @param {string} numero2 - Numero 2
 	 * @returns {number} - Valores somados
+	 * @see {@link https://www.google.com/}
 	 */
 	soma(numero, numero2) {
 		return numero + numero2;
+	}
+
+	/**	
+	*Subtrai o segundo valor do primeiro
+	*@param {number} first - Primeiro valor
+	*@param {number} second - Segundo valor
+	*@returns {number} - Resultado
+	*/
+	subtrai(first, second){
+		return first - second;
 	}
 
 }
